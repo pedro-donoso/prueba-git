9c458819536bba932b3de075183e7b0bef3572bb
Pedro Donoso <pedro.donoso.r@usach.cl>
2024-06-04 13:04:18 -0400

    agrego carpetas del proyecto
    del archivo descargado agrego los archivos de las carpetas

---
+++ app/src/main/java/com/example/login10/view/login/LoginFragment.kt
@@ -0,0 +1,44 @@
+package com.example.login10.view.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.login10.R
+import com.example.login10.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Navegar al HomeFragment al hacer clic en el botón de login
+        binding.loginButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+        }
+
+        // Navegar al RegisterFragment al hacer clic en el botón de register
+        binding.registerButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
